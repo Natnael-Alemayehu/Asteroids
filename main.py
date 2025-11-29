32f2c96e9cc9b9dd4488ffbfd673cb2cc1fbdd6d
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2025-11-29 13:32:07 +0300

    player sprite added to the game loop

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,7 +20,13 @@ def main():
                 return
 
         screen.fill("black")
+        
+        player.draw(screen)    
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 
